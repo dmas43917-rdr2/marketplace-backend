a846e631e20657a4c773af05b87c66b417362c11
Dani <dmas43917@gmail.com>
2026-05-30 17:48:36 +0700

    before move logger

--- config/logger.js
+++ config/logger.js
@@ -4,7 +4,7 @@ const logger = createLogger({
     level: 'info',
     format: format.combine(
         format.timestamp(),
-        format.printf(({ level, message, timestamp }) => {
+        format.json(({ level, message, timestamp }) => {
             return `${timestamp} [${level.toUpperCase()}] ${message}`;
         })
     ),
